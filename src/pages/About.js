7f606ef30af05aeb3f980b476efe49d342955724
Danny Phillips <danny.phillips8@gmail.com>
2019-10-08 13:47:25 -0700

    fix: lint errors

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => (
   <div>
+    <Link />
     <h2>About</h2>
   </div>
-)
-export default About
+);
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => (
   <div>
+    <Link />
     <h2>Home</h2>
   </div>
-)
-export default Home
+);
+export default Home;
